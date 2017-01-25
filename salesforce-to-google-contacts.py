a9fc6e5372321edac39c0935fd000fd947827ed7
Philippe Lemaire <lemaire.philippe@gmail.com>
2017-01-25 19:06:41 +0100

    don't repeat yourself

--- salesforce-to-google-contacts.py
+++ salesforce-to-google-contacts.py
@@ -12,34 +12,34 @@ report = input("Quel est le report à traiter ?\n")
 sourcefile = open(report, 'r')
 sourcereader = csv.reader(sourcefile, delimiter=';', quotechar='"')
 
-# Let's open our template google contacts file
+# Let's create our template google contacts file
 
 exportfilename = 'contacts' + '_' + str(datetime.datetime.now())[:-7] + '.csv'
 contactsfile = open(exportfilename, 'w')
 contactswriter = csv.writer(contactsfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+# first line of the contacts csv file
 contactswriter.writerow(['Name','Given Name','Family Name','Group Membership','E-mail 1 - Type','E-mail 1 - Value','Phone 1 - Type','Phone 1 - Value','Phone 2 - Type', 'Phone 2 - Value'])
 
+# trick to skip the first line in the source file
 firstline = True
 counter = 0
 
+def fix_number(phone_number):
+    #fix phone numbers that don't start with 0'
+    if phone_number.startswith('33'):
+        phone_number = "0" + phone_number[2:]
+    elif phone_number.startswith('6') or phone_number.startswith('7') or phone_number.startswith('8') or phone_number.startswith('9'):
+        phone_number = "0" + phone_number
+    return phone_number
+
+
 for row in sourcereader:
-# skip the first line
     if firstline:
         firstline = False
         continue
-    #fix phone numbers that don't start with 0'
-    if row[8].startswith('33'):
-        row[8] = "0" + row[8][2:]
-    elif row[8].startswith('6') or row[8].startswith('7') or row[8].startswith('8') or row[8].startswith('9'):
-        row[8] = "0" + row[8]
 
-    if row[9].startswith('33'):
-        row[9] = "0" + row[9][2:]
-    elif row[9].startswith('6') or row[9].startswith('7') or row[9].startswith('8') or row[9].startswith('9'):
-        row[9] = "0" + row[9]
-
-    stuff = [" ".join([row[1].title(),row[2].title()]),row[1].title(),row[2].title(),'Praticiens','* Work',row[10],'Work',row[8],'Mobile',row[9]]
+    stuff = [" ".join([row[1].title(),row[2].title()]),row[1].title(),row[2].title(),'Praticiens','* Work',row[10],'Work',fix_number(row[8]),'Mobile',fix_number(row[9])]
     contactswriter.writerow(stuff)
     counter += 1
 
