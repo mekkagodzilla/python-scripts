3c5ba2ccd85a0f78e73832a613a6f338fbd01380
Philippe Lemaire <lemaire.philippe@gmail.com>
2017-07-21 16:44:49 +0200

    pffft

--- pagesjaunes-scrap.py
+++ pagesjaunes-scrap.py
@@ -1,39 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 # premier script pour manipuler du contenu html avec beautifulsoup.
 # objectif : sortir une liste de prats réservables sur pjd
 
 import requests, bs4, csv, datetime
 
-spes = ['osteopathe', 'podologue', 'medecin', 'dentiste']
+spes = ['osteopathe', 'podologue', 'medecin', 'dentiste', 'pneumologue', 'cardiologue', 'gynecologue', 'ophtalmologue', 'orl', 'dermatologue']
 
-# Créons notre fichier de résultat csv
+departements = ['rhone-69','isere-38','loire-42','drome-26','haute-loire-43','ardeche-07']
 
-resultFileName = 'scrap-pj' + '_' + str(datetime.datetime.now())[:-7] + '.csv'
-result = open(resultFileName, 'w')
-resultWriter = csv.writer(result, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-# Contenu de la première ligne du tableau de résultats
-resultWriter.writerow(['Spécialité','Nom','Téléphone','Adresse'])
+for departement in departements:
+    # Créons notre fichier de résultat csv
 
-compteur = 0
+    resultFileName = 'scrap-pj' + '_' + str(datetime.datetime.now())[:-7] + '_' + departement +'.csv'
+    result = open(resultFileName, 'w')
+    resultWriter = csv.writer(result, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-for spe in spes:
-    url = 'https://www.pagesjaunes.fr/recherche/departement/rhone-69/' + spe
-    res = requests.get(url)
-    soup = bs4.BeautifulSoup(res.text, 'lxml')
-    prats = soup.select("div.zone-produit")
+    # Contenu de la première ligne du tableau de résultats
+    resultWriter.writerow(['Spécialité','Nom','Téléphone','Adresse'])
 
-    for prat in prats:
-        nom = prat.parent.a.get_text() 
+    compteur = 0
 
-        try:
-            tel = prat.parent.find("strong", class_="num").get_text()
-        except AttributeError:
-            tel = "Non trouvé"
-        adresse = prat.parent.find("a", class_="adresse").get_text()
+    for spe in spes:
+        url = 'https://www.pagesjaunes.fr/annuaire/departement/' + departement +'/' + spe
+        res = requests.get(url)
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        prats = soup.select("div.zone-produit")
 
-        myRow = [spe, nom, tel, adresse]
+        for prat in prats:
+            nom = prat.parent.a.get_text() 
 
-        resultWriter.writerow(myRow)
-        compteur += 1
+            try:
+                tel = prat.parent.find("strong", class_="num").get_text()
+            except AttributeError:
+                tel = "Non trouvé"
+            adresse = prat.parent.find("a", class_="adresse").get_text()
 
-print('Travail terminé, {0} prats ont été scrapés.'.format(compteur))
+            myRow = [spe, nom, tel, adresse]
+
+            resultWriter.writerow(myRow)
+            compteur += 1
+
+    print('Travail terminé sur', departement, ', {0} prats ont été scrapés.'.format(compteur))
